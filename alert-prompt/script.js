8e1cb55ae79ee0afc021fb183ecee105b3933574
Şamxal Əhmədov <101803952+samxalehmedov@users.noreply.github.com>
2022-05-09 03:29:34 -0700

    Add files via upload

---
+++ alert-prompt/script.js
@@ -0,0 +1,3 @@
+let cavab =  prompt('Js ne zaman yaranib?');
+let netice = cavab == 1995 ? "Cavab duzgundur." : cavab <1995 && cavab >= 1990 ? "Yaxinlasdiniz" : cavab <=2000 && cavab >1995 ? "Uazaqlasdiniz" : "Yanlisdir";
+alert(netice);
